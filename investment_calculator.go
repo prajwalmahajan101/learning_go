dff2ddcde9e9e2bb31f41acfce7545875b55a88e
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2024-08-04 11:43:21 +0530

    formatted string

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,22 +7,22 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	
+
 	var investmentAmount, years, expectedReturnRate float64
 
 	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
-	
+
 	fmt.Print("Expected ReturnRate: ")
 	fmt.Scan(&expectedReturnRate)
-	
+
 	fmt.Print("Years: ")
 	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
-
-	fmt.Println("Future value: ", futureValue)
-	fmt.Println("Future real value: ", futureRealValue)
+	formattedFV := fmt.Sprintf("Future value: %.2f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future real value: %.2f \n", futureRealValue)
+	fmt.Print(formattedFV, formattedRFV)
 }
